2f257012a4735a9309327b9bfda746ad040e2e94
arjunKa <32141878+arjunKa@users.noreply.github.com>
2023-06-30 00:39:46 -0400

    File path can be selected, with custom name and last name
    include exe file for desktop use

---
+++ fileManage.py
@@ -0,0 +1,27 @@
+from tkinter.filedialog import askdirectory
+from datetime import datetime
+import os
+
+x = datetime.now()
+
+class FileManage():
+    def __init__(self):
+        self.path = ''
+    
+fm = FileManage()
+
+def chooseFolder():
+    path = askdirectory(title='Select Folder') # shows dialog box and return the path
+    fm.path = path
+    return path
+    print(fm.path)  
+
+    
+    
+    
+def createFile(fname, lname):
+    x = datetime.now()
+    file_path = os.path.join(fm.path, 'Resume_' + fname.strip().replace(" ", "")+ lname.strip().replace(" ", "") + "_" + x.strftime('%d-%m-%Y.docx'))
+    print(fm.path)
+    f = open(file_path, 'a')
+    
---
+++ main.py
@@ -0,0 +1,99 @@
+from tkinter import END, Tk, Label, Button, Text, Scrollbar, RIGHT, Y
+
+from fileManage import chooseFolder, createFile
+
+class mainGUI():
+    
+    
+    
+    def __init__(self, master):
+        padding = 3
+        
+        width = 400 # Width 
+        height = 300 # Height
+
+        screen_width = root.winfo_screenwidth()  # Width of the screen
+        screen_height = root.winfo_screenheight() # Height of the screen
+        
+        # Calculate Starting X and Y coordinates for Window
+        x = (screen_width/2) - (width/2)
+        y = (screen_height/2) - (height/2)
+        
+        self.master = master
+        master.title("CV Automation")
+        master.geometry('%dx%d+%d+%d' % (width, height, x, y))
+        # master.eval('tk::PlaceWindow . center')
+        
+        
+        
+        l = Label(root, text = "First Name")
+        l.config(font =("Courier", 10))
+
+        T = Text(master, height = 1, width = 25)
+        T.insert(END, "First Name")
+        # T.bind('<KeyRelease>',self.my_upd)
+        self.fname = T
+        
+        l2 = Label(root, text = "Last Name")
+        l2.config(font =("Courier", 10))
+
+        T2 = Text(master, height = 1, width = 25)
+        T2.insert(END, "Last Name")
+        # T.bind('<KeyRelease>',self.my_upd)
+        self.lname = T2
+
+
+        self.gen_file_button = Button(master, text="Update", command=self.gen_file)
+
+        
+        self.label = Label(master, text="This is our first GUI!")
+
+        l3 = Label(root, text = "No path selected", bg = "white")
+        # T3.insert(END, "File Path")
+        self.path = l3
+
+        self.choose_path_button = Button(master, text="Choose Path", command=self.choose_path)
+
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        
+        
+        # for child in master.winfo_children():
+        #     child.config(padx=10, pady=10)
+            # child.place(relx = .5, rely = 0, anchor="center")
+        
+        l.pack(padx=padding, pady=padding)
+        self.fname.pack(padx=padding, pady=padding)
+        self.lname.pack(padx=padding, pady=padding)
+
+        self.label.pack(padx=padding, pady=padding)
+        self.path.pack(padx=padding, pady=padding)
+        self.choose_path_button.pack(padx=padding, pady=padding)
+        self.gen_file_button.pack(padx=padding, pady=padding)
+        self.close_button.pack(padx=padding, pady=padding+10)
+        
+        
+    def choose_path(self):
+        print("choose folder")
+        self.path.config(text = chooseFolder())
+        
+    def gen_file(self):
+        fname = self.fname.get("1.0",'end-1c')
+        lname = self.lname.get("1.0",'end-1c')
+        createFile(fname, lname)
+        
+    
+        
+    
+        
+    def my_upd(self, value):
+        my_str= self.fname.get("1.0",'end-1c') #The input string except the last line break
+        breaks=my_str.count('\n') # Number of line breaks ( except the last one )
+        char_numbers=len(my_str)-breaks # Number of chars user has entered 
+        # l2.config(text=str(char_numbers)) # display number of chars 
+        if(char_numbers > 7):
+            self.fname.delete('end-{}c'.format(char_numbers-7)) # remove last char of text widget
+
+root = Tk()
+my_gui = mainGUI(root)
+root.mainloop()
