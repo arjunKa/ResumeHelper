a4af97ff06c2a1f935db1c54011bc67a38525ca1
arjunKa <32141878+arjunKa@users.noreply.github.com>
2023-06-30 16:16:35 -0400

    added frames to GUI, and new exe

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import END, Tk, Label, Button, Text, Scrollbar, RIGHT, Y
+from tkinter import BOTTOM, END, Frame, Tk, Label, Button, Text, Scrollbar, RIGHT, Y
 
 from fileManage import chooseFolder, createFile
 
@@ -6,6 +6,11 @@ from fileManage import chooseFolder, createFile
 class mainGUI:
     def __init__(self, master):
         padding = 3
+
+        frame = Frame(root)
+        footer_frame = Frame(root)
+        frame.pack()
+        footer_frame.pack(side=BOTTOM)
 
         width = 400  # Width
         height = 300  # Height
@@ -22,35 +27,37 @@ class mainGUI:
         master.geometry("%dx%d+%d+%d" % (width, height, x, y))
         # master.eval('tk::PlaceWindow . center')
 
-        l = Label(root, text="First Name")
+        l = Label(frame, text="Enter Name:")
         l.config(font=("Courier", 10))
 
-        T = Text(master, height=1, width=25)
+        T = Text(frame, height=1, width=25)
         T.insert(END, "First Name")
         # T.bind('<KeyRelease>',self.my_upd)
         self.fname = T
 
-        l2 = Label(root, text="Last Name")
+        l2 = Label(frame, text="Last Name")
         l2.config(font=("Courier", 10))
 
-        T2 = Text(master, height=1, width=25)
+        T2 = Text(frame, height=1, width=25)
         T2.insert(END, "Last Name")
         # T.bind('<KeyRelease>',self.my_upd)
         self.lname = T2
 
-        self.gen_file_button = Button(master, text="Update", command=self.gen_file)
+        self.gen_file_button = Button(
+            frame, text="Generate File", command=self.gen_file
+        )
 
-        self.label = Label(master, text="This is our first GUI!")
+        self.label = Label(frame, text="This is our first GUI!")
 
-        l3 = Label(root, text="No path selected", bg="white")
+        l3 = Label(frame, text="No path selected", bg="white")
         # T3.insert(END, "File Path")
         self.path = l3
 
         self.choose_path_button = Button(
-            master, text="Choose Path", command=self.choose_path
+            frame, text="Choose Path", command=self.choose_path
         )
 
-        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button = Button(footer_frame, text="Close", command=master.quit)
 
         # for child in master.winfo_children():
         #     child.config(padx=10, pady=10)
@@ -64,7 +71,7 @@ class mainGUI:
         self.path.pack(padx=padding, pady=padding)
         self.choose_path_button.pack(padx=padding, pady=padding)
         self.gen_file_button.pack(padx=padding, pady=padding)
-        self.close_button.pack(padx=padding, pady=padding + 10)
+        self.close_button.pack(padx=padding, pady=padding + 10, side=BOTTOM)
 
     def choose_path(self):
         print("choose folder")
